d107040a92018e122d2d03e891fdf3aeb7483829
olivier-sonrel <o.sonrel@it-students.fr>
2022-03-31 09:14:40 +0200

    start pojo

---
+++ app/src/main/java/com/olivier/mybestyoutube/YoutubeVideo.java
@@ -0,0 +1,13 @@
+package com.olivier.mybestyoutube;
+
+import java.net.URL;
+
+public class YoutubeVideo {
+
+    private Long id;
+    private String title;
+    private String description;
+    private URL url;
+    private String category;
+
+}
