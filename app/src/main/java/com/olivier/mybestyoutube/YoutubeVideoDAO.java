d40be0f1566477012f639c9fa899d14e6c855973
olivier-sonrel <o.sonrel@it-students.fr>
2022-03-31 11:02:24 +0200

    done YoutubeVideoDAO::list() done

--- app/src/main/java/com/olivier/mybestyoutube/YoutubeVideoDAO.java
+++ app/src/main/java/com/olivier/mybestyoutube/YoutubeVideoDAO.java
@@ -3,6 +3,9 @@ package com.olivier.mybestyoutube;
 import android.content.Context;
 import android.database.Cursor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class YoutubeVideoDAO extends DAO{
     public YoutubeVideoDAO(Context context){ super(new YoutubeVideoDBHelper(context)); }
 
@@ -25,5 +28,33 @@ public class YoutubeVideoDAO extends DAO{
             cursor.close();
         }
         return youtubeVideo;
+    }
+
+    public List<YoutubeVideo> list(){
+        open();
+
+        List<YoutubeVideo> youtubeVideos = new ArrayList<>();
+
+        Cursor cursor = db.rawQuery("select * from " + YoutubeVideoDBHelper.YOUTUBE_VIDEO_TABLE_NAME, null);
+
+        if(cursor.moveToFirst()){
+            while (!cursor.isAfterLast()){
+                YoutubeVideo youtubeVideo = new YoutubeVideo();
+
+                youtubeVideo.setId(cursor.getLong(YoutubeVideoDBHelper.YOUTUBE_VIDEO_KEY_COLUMN_INDEX));
+                youtubeVideo.setTitle(cursor.getString(YoutubeVideoDBHelper.YOUTUBE_VIDEO_TITLE_COLUMN_INDEX));
+                youtubeVideo.setDescription(cursor.getString(YoutubeVideoDBHelper.YOUTUBE_VIDEO_DESCRIPTION_COLUMN_INDEX));
+                youtubeVideo.setUrl(cursor.getString(YoutubeVideoDBHelper.YOUTUBE_VIDEO_URL_COLUMN_INDEX));
+                youtubeVideo.setCategory(cursor.getString(YoutubeVideoDBHelper.YOUTUBE_VIDEO_CATEGORY_COLUMN_INDEX));
+
+                youtubeVideos.add(youtubeVideo);
+
+                cursor.moveToNext();
+            }
+        }
+
+        cursor.close();
+
+        return youtubeVideos;
     }
 }
