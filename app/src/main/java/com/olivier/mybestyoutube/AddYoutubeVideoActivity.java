b80f50c5431496ae7bfca837350febb36989f092
olivier-sonrel <o.sonrel@it-students.fr>
2022-03-31 15:03:59 +0200

    maj AndroidManifest.xml back button et spinner_item affichage ok

--- app/src/main/java/com/olivier/mybestyoutube/AddYoutubeVideoActivity.java
+++ app/src/main/java/com/olivier/mybestyoutube/AddYoutubeVideoActivity.java
@@ -2,13 +2,58 @@ package com.olivier.mybestyoutube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AddYoutubeVideoActivity extends AppCompatActivity {
+
+    private EditText etTitle;
+    private EditText etDescription;
+    private EditText etUrl;
+    private Spinner spCategory;
+    private Button btnAdd;
+    private Button btnCancel;
+    private Context context;
+    private String[] categories;
+
+    public static String[] getCategories(){
+        return new String[]{
+                "Sport",
+                "Music",
+                "Comedy",
+                "Cooking"
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_youtube_video);
+
+        etTitle = findViewById(R.id.etTitle);
+        etDescription = findViewById(R.id.etDescription);
+        etUrl = findViewById(R.id.etUrl);
+        spCategory = findViewById(R.id.spCategory);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        categories = getCategories();
+
+        context = getApplicationContext();
+
+        final List<String> categoriesList = new ArrayList<>(Arrays.asList(categories));
+
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(context , R.layout.spinner_item , categoriesList);
+
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spCategory.setAdapter(spinnerArrayAdapter);
     }
 }
