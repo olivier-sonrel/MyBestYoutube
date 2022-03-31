ca3c9b1c4687b465ba1ecbf8c842f07ec4c92fc5
olivier-sonrel <o.sonrel@it-students.fr>
2022-03-31 14:26:30 +0200

    menu done

--- app/src/main/java/com/olivier/mybestyoutube/MainActivity.java
+++ app/src/main/java/com/olivier/mybestyoutube/MainActivity.java
@@ -1,14 +1,44 @@
 package com.olivier.mybestyoutube;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+        //return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itAddYoutubeVideo:
+
+                Intent intent = new Intent(context, AddYoutubeVideoActivity.class);
+
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
     }
 }
